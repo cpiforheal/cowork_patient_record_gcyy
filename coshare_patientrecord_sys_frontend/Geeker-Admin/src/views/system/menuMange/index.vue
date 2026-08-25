50cccf8d6acbaa145b72a4fc316bc58baa6350c4
cpiforheal <1219222654@qq.com>
2026-08-25 14:47:21 +0800

    兼容旧系统管理动态路由

---
+++ coshare_patientrecord_sys_frontend/Geeker-Admin/src/views/system/menuMange/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <RoleManagePage />
+</template>
+
+<script setup lang="ts">
+import RoleManagePage from "../roleManage/index.vue";
+</script>
---
+++ coshare_patientrecord_sys_frontend/Geeker-Admin/src/views/system/systemLog/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <AuditLogPage />
+</template>
+
+<script setup lang="ts">
+import AuditLogPage from "../../audit/log/index.vue";
+</script>
